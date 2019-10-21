8f1c715f75332333a15421eca7ef74c5fc6cdd4f
DevelopmentByDavid <dsilv022@ucr.edu>
2019-10-21 13:28:20 -0700

    udpated compatibility

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,12 +30,13 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.json({ message: 'Hello World'})
+    // res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+    // // render the error page
+    // res.status(err.status || 500);
+    // res.render('error');
 });
 
 module.exports = app;
