ffb4d2f3a2d5cd0919643c48aeaeeab3305cfdbe
Alexander Georgalakis <ageor009@Alexanders-MacBook-Pro.local>
2019-11-23 12:19:56 -0800

    added commits

---
+++ server/lib/conflicts.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {*} classes - this will be an array with the properties classes.days and classes.times.
+ * possible values for day: 'MWF' or 'TTH'
+ * possible values for times: date obj
+ * possibly use date-fns
+ */
+
+export function checkTimeConflict(classes){
+    
+}
+
+//classes.dependencies.co
+export function checkCoReq(classes){}
+
+//probably going to make db call to the read
+//also since I am making a db call I would need to a 'next imoport' 
+export function checkPreReq(classes){}
+
+
+
+
+
