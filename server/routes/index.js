522d1d3db36b6015f1c93d356abab18be81d0174
DevelopmentByDavid <david@yrbookit.com>
2019-11-26 21:39:55 -0800

    fixed faker

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
-const router = express.Router();
-
 import faker from 'faker';
+
+const router = express.Router();
 
 const conflicts = require('../lib/conflicts');
 
 const genOfferings = () => ({
     courseId: faker.random.number(), // will be redefined later
-    //teacher: null, // will be redefined later
-    //capacity: faker.random.number(),
-    //location: `${faker.hacker.noun()} ${faker.random.number(400)}`,
-    //start: chooseMeeting(faker.random.number(1)),
+    teacher: null, // will be redefined later
+    capacity: faker.random.number(),
+    location: `${faker.hacker.noun()} ${faker.random.number(400)}`,
+    // start: chooseMeeting(faker.random.number(1)),
     days: 'MWF',
     start: faker.date.recent(),
-    end: faker.date.soon(),
+    end: faker.date.soon()
     //quarter: `${chooseQuarter(faker.random.number(2))}19`
 });
 
-function generateObj(){
+function generateObj() {
     const temp = [];
-    for (let i = 0; i < 100; i += 1){
-        console.log("hello");
+    for (let i = 0; i < 100; i += 1) {
+        console.log('hello');
         temp.push(genOfferings());
     }
     console.log(temp);
@@ -28,14 +29,11 @@ function generateObj(){
 }
 
 router.get('/', function(req, res, next) {
-    console.log("starting executing");
+    console.log('starting executing');
 
-    console.log(faker.random.number());
-    console.log(faker.date.recent());
-    console.log(faker.date.soon());
-    //conflicts.courseConflictMsg(generateObj());
-  });
+    conflicts.courseConflictMsg(generateObj());
+});
 
 // router.get('/', conflicts.courseConflictMsg);
-  
+
 module.exports = router;
