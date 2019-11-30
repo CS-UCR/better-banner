68b5634d6ef1db69b7d05cb7e62d863d0a973bb8
DevelopmentByDavid <david@yrbookit.com>
2019-11-29 23:11:52 -0800

    formatting + modified useFetch to in sync with plain fetch

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 
 // TODO: make a way to rerun the fetch other than refreshing the page
-export default function useFetch(url) {
+export default function useFetch(...args) {
     const [data, setData] = useState({});
     const [loading, setLoading] = useState(true);
 
     // this hook only runs the first time because of the empty array dependency list
     useEffect(() => {
         // get information on user #1
-        fetch('/api/sandbox', {
+        fetch(...args, {
             method: 'GET'
         })
             .then(res => {
@@ -19,7 +19,7 @@ export default function useFetch(url) {
             })
             // eslint-disable-next-line
             .catch(e => console.log(e));
-    }, [url]);
+    }, []);
 
     return [loading, data];
 }
